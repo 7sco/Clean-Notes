a0bb3a2950877fd50895f19633b194836495a8d1
mitch <mitch@tabian.ca>
2020-03-30 13:26:51 -0700

    dynamic navigation with multiple modules

--- settings/src/main/java/com/codingwithmitch/cleannotes/settings/presentation/SettingsFragment.kt
+++ settings/src/main/java/com/codingwithmitch/cleannotes/settings/presentation/SettingsFragment.kt
@@ -1,10 +1,22 @@
 package com.codingwithmitch.cleannotes.settings.presentation
 
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.codingwithmitch.settings.R
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        settings_fragment_title.setOnClickListener {
+
+        }
+    }
 }
